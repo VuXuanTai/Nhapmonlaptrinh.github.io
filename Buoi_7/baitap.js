5d79ee6bd33afaa5254e6c7a11194da1d53a60ba
VuXuanTai <taivu.xd.k64@gmail.com>
2021-07-15 18:50:18 +0700

    Bài tập

---
+++ Buoi_7/baitap.js
@@ -0,0 +1,42 @@
+function tonghaiso(){
+      let a = prompt('nhập số nguyên a: ');
+      let b = prompt('nhập số nguyên b: ');
+      a = parseInt(a);
+      b = parseInt(b);
+      let c = (a + b);
+      alert('tổng hai số a và b là: ' + c);
+}
+function hienthi()
+{
+      let day = prompt('nhập ngày: ');
+      let month = prompt('nhập tháng: ');
+      let year = prompt('nhập năm: ')
+      alert('Hôm nay là: ' + day + '/' + month + '/' + year);
+}
+function tinhtoan()
+{
+      const pi = 3.14;
+      let r = prompt('Nhập bán kính hình tròn: ');
+      let cv = (2*r*pi); // tính chu vi hình tròn
+      let dt = (r*r*pi); // tính diện tích hình tròn
+      alert('Hình tròn có chu vi: ' + cv + ' và diện tích: ' + dt);
+}
+function tinhlaisuat()
+{
+      let tiengui = prompt('Nhập số tiền gửi: ');
+      let laisuat = prompt('Nhập lãi suất: ');
+      let nam = prompt('Nhập số năm: ')
+      nam = parseInt(nam);
+      laisuat = parseFloat(laisuat);
+      tiengui = parseInt(tiengui);
+      let tongtien = (tiengui + nam * (tiengui*(laisuat/100)));
+      alert('Tổng tiền ( gốc + lãi): ' + tongtien);
+}
+function inthu()
+{
+      let name = prompt('Nhập tên người nhận: ');
+      let address = prompt('Nhập địa điểm: ');
+      let time = prompt('Nhập thời gian: ');
+      let noidung = prompt('Nhập nội dung thư: ');
+      alert(name + ' thương nhớ, \n' + noidung + '\n' + address + ', ' + time)
+}
---
+++ F8_learn/main.js
@@ -0,0 +1,23 @@
+var fullName = "Vu Xuan Tai";
+
+var age = 20
+alert(fullName)
+alert(age)
+/*
+Gioi thieu ham built-in
+      1. Alert
+      2. Console
+      3. Confirm
+      4. Prompt
+      5. Set timeout
+      6. Set interval
+*/
+console.log(fullName)
+confirm('xac nhan ban du tuoi!')
+prompt('xac nhan ban du tuoi!')
+setTimeout(function() {
+      alert('thong bao')
+}, 1000)
+setInterval(function() {
+      console.log('day la log')      
+}, 1000)
